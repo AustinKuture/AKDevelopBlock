24e8bee757595b1747b37276b2264b57b5728c06
AustinKuture <ya_kun_13@163.com>
2017-06-18 11:56:14 +0800

    添加搜索控制器,加入图片选择器

---
+++ AKDevelopBlock/AKDevelopBlock/AKPickImgController.h
@@ -0,0 +1,13 @@
+//
+//  AKPickImgController.h
+//  AKDevelopBlock
+//
+//  Created by 李亚坤 on 2017/6/18.
+//  Copyright © 2017年 Kuture. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AKPickImgController : UITableViewController
+
+@end
--- AKDevelopBlock/AKDevelopBlock/ViewController.m
+++ AKDevelopBlock/AKDevelopBlock/ViewController.m
@@ -8,13 +8,17 @@
 
 #import "ViewController.h"
 #import "UIViewController+alvertVC.h"
+#import "AKSearchResultController.h"
+#import "PYSearch.h"
 
-@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource,PYSearchViewControllerDelegate>
 
 @property (nonatomic,strong) UITableView *typeTableView;
 
 @property (nonatomic,strong) NSArray *typeArray;
 @property (nonatomic,strong) NSArray *classTypeArray;
+
+@property (nonatomic,strong) UINavigationController *nav;
 
 @end
 
@@ -31,8 +35,8 @@
 #pragma mark ***数据初始化***
 - (void)parmatersInitSetting{
     
-    _typeArray = @[@"知识类图文视图",@"知识类文本视图",@"图像嵌入文本类视图",@"图像浏览与动画加载类视图",@"登陆类视图样式一",@"登陆类视图样式二",@"无限轮播类视图",@"控制器内内弹窗",@"图像比例拉伸",@"图像蒙版",@"生成指定颜色图片",@"色彩渐变",@"微博功能按钮",@"缓存扫描及清理",@"快捷视图窗口",@"缩放旋转及拖动手势",@"3D标签"];
-    _classTypeArray = @[@"AKViewAndTextViewController",@"AKKonwTextViewController",@"AKPhotoAndTextViewController",@"AKPhotoAndAnimationController",@"AKLoginTableView",@"AKLoginTableViewTwo",@"AKSDCycleViewController",@"",@"AKImgResizeOne",@"AKPicBlurViewController",@"AKColorsImageView",@"AKColorFade",@"AKWeiblogToolBar",@"AKCacheClean",@"AKPlusBtnController",@"AKGestureSeris",@"AK3DLabelView"];
+    _typeArray = @[@"知识类图文视图",@"知识类文本视图",@"图像嵌入文本类视图",@"图像浏览与动画加载类视图",@"登陆类视图样式一",@"登陆类视图样式二",@"无限轮播类视图",@"控制器内内弹窗",@"图像比例拉伸",@"图像蒙版",@"生成指定颜色图片",@"色彩渐变",@"微博功能按钮",@"缓存扫描及清理",@"快捷视图窗口",@"缩放旋转及拖动手势",@"3D标签",@"图片选择器",@"搜索控制器"];
+    _classTypeArray = @[@"AKViewAndTextViewController",@"AKKonwTextViewController",@"AKPhotoAndTextViewController",@"AKPhotoAndAnimationController",@"AKLoginTableView",@"AKLoginTableViewTwo",@"AKSDCycleViewController",@"",@"AKImgResizeOne",@"AKPicBlurViewController",@"AKColorsImageView",@"AKColorFade",@"AKWeiblogToolBar",@"AKCacheClean",@"AKPlusBtnController",@"AKGestureSeris",@"AK3DLabelView",@"AKPickImgController",@"AKSearchTableController"];
 }
 
 #pragma mark ***类型与模块**
@@ -91,6 +95,9 @@
         tabVC.title = _typeArray[indexPath.row];
         [self presentViewController:tabVC animated:YES completion:nil];
         
+    }else if (indexPath.row == 18){
+        
+        [self searchBugClickBtn];
     }else{
         
         UITableViewController *tabVC = [[NSClassFromString(_classTypeArray[indexPath.row]) alloc]init];
@@ -105,8 +112,58 @@
     
 }
 
+#pragma mark ***Bug搜索***
+- (void)searchBugClickBtn{
+    
+    // 1.创建热门搜索
+    NSArray *hotSeaches = @[@"Java",@"iOS",@"Html",@"Php",@"Swift",@"Ruby",@"C",@"Python",@"C++",@"Go",@"睿思鸣"];
+    
+    // 2. 创建控制器
+    PYSearchViewController *searchViewController = [PYSearchViewController searchViewControllerWithHotSearches:hotSeaches searchBarPlaceholder:@"请输入您要搜索的内容" didSearchBlock:^(PYSearchViewController *searchViewController, UISearchBar *searchBar, NSString *searchText) {
+        
+        // 开始搜索执行以下代码
+        // 设置搜索结果控制器标题
+        
+        AKSearchResultController *searchVC = [AKSearchResultController new];
+        searchVC.title = @"搜索结果";
+        searchVC.searchWords = searchText;
+        [_nav pushViewController:searchVC animated:YES];
+    }];
+    
+    // 3. 设置风格
+    searchViewController.hotSearchStyle = PYHotSearchStyleDefault; // 热门搜索风格为默认
+    searchViewController.searchHistoryStyle = 2; // 搜索历史风格根据选择
+    
+    
+    // 4. 设置代理
+    searchViewController.delegate = self;
+    
+    // 5. 跳转到搜索控制器
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:searchViewController];
+    _nav = nav;
+    
+//    _nav.navigationBar.barTintColor = BLUE_HARD;
+//    [_nav.navigationBar setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIColor whiteColor],NSForegroundColorAttributeName,nil]];
+    
+    [self presentViewController:_nav  animated:NO completion:nil];
+}
 
 
+- (void)searchViewController:(PYSearchViewController *)searchViewController didSelectHotSearchAtIndex:(NSInteger)index searchText:(NSString *)searchText{
+    
+    AKSearchResultController *searchVC = [AKSearchResultController new];
+    searchVC.title = @"搜索结果";
+    searchVC.searchWords = searchText;
+    [_nav pushViewController:searchVC animated:YES];
+}
+
+- (void)searchViewController:(PYSearchViewController *)searchViewController didSelectSearchHistoryAtIndex:(NSInteger)index searchText:(NSString *)searchText{
+    
+    AKSearchResultController *searchVC = [AKSearchResultController new];
+    searchVC.title = @"搜索结果";
+    searchVC.searchWords = searchText;
+    [_nav pushViewController:searchVC animated:YES];
+}
 
 
 
